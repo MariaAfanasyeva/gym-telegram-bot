773e42a530d53a8e15dc5379be1c2e9a9ea877d9
Maryia Afanasyeva <Maryia.Afanasyeva@itechart-group.com>
2022-06-02 16:49:05 +0300

    Added main function

--- bot.py
+++ bot.py
@@ -5,7 +5,8 @@ from telegram.ext.commandhandler import CommandHandler
 from telegram.ext.messagehandler import MessageHandler
 from telegram.ext.filters import Filters
 import os
-PORT = int(os.environ.get('PORT', 5000))
+
+PORT = int(os.environ.get('PORT', 80))
 TOKEN = "5404507587:AAEc3mmotE7zIg5Hdza_TQBk6hfuqB2jQDs"
 updater = Updater(TOKEN,
                   use_context=True)
@@ -55,20 +56,23 @@ def unknown_text(update: Update, context: CallbackContext):
     update.message.reply_text(
         "Sorry I can't recognize you , you said '%s'" % update.message.text)
   
-  
-updater.dispatcher.add_handler(CommandHandler('start', start))
-updater.dispatcher.add_handler(CommandHandler('youtube', youtube_url))
-updater.dispatcher.add_handler(CommandHandler('help', help))
-updater.dispatcher.add_handler(CommandHandler('linkedin', linkedIn_url))
-updater.dispatcher.add_handler(CommandHandler('gmail', gmail_url))
-updater.dispatcher.add_handler(CommandHandler('geeks', geeks_url))
-updater.dispatcher.add_handler(MessageHandler(Filters.text, unknown))
-updater.dispatcher.add_handler(MessageHandler(
+def main():
+    updater.dispatcher.add_handler(CommandHandler('start', start))
+    updater.dispatcher.add_handler(CommandHandler('youtube', youtube_url))
+    updater.dispatcher.add_handler(CommandHandler('help', help))
+    updater.dispatcher.add_handler(CommandHandler('linkedin', linkedIn_url))
+    updater.dispatcher.add_handler(CommandHandler('gmail', gmail_url))
+    updater.dispatcher.add_handler(CommandHandler('geeks', geeks_url))
+    updater.dispatcher.add_handler(MessageHandler(Filters.text, unknown))
+    updater.dispatcher.add_handler(MessageHandler(
     Filters.command, unknown))  # Filters out unknown commands
   
-# Filters out unknown messages.
-updater.dispatcher.add_handler(MessageHandler(Filters.text, unknown_text))
+    # Filters out unknown messages.
+    # updater.dispatcher.add_handler(MessageHandler(Filters.text, unknown_text))
+    # updater.start_polling()
+    
+    updater.start_webhook(listen='0.0.0.0', port=PORT,url_path=TOKEN, webhook_url='https://gym-my-telegram-bot.herokuapp.com/' + TOKEN)
+    updater.bot.setWebhook('https://gym-my-telegram-bot.herokuapp.com/' + TOKEN)
 
-updater.start_webhook(url_path=TOKEN, 
-                          webhook_url='https://gym-my-telegram-bot.herokuapp.com/' + TOKEN)
-# updater.bot.setWebhook('https://gym-my-telegram-bot.herokuapp.com/' + TOKEN)
+if __name__ == "__main__":
+    main()
